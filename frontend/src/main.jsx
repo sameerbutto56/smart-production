5bac079d4a62537ccca2ebd7cdd81f8e6091a9ad
Sameer Butt <sameerbutt056@gmail.com>
2026-04-27 13:50:49 +0500

    Deployment Trigger: Final Rebranding

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Deploy Sync: 2026-04-27 - Final Rebranding & Command Center
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
