ae076ffa3ae07721a3626f20f2f3ae03745e7be4
Sameer Butt <sameerbutt056@gmail.com>
2026-05-06 09:19:50 +0500

    Fix: Resolved syntax error in AllOrders.jsx that blocked production build

--- frontend/src/pages/AllOrders.jsx
+++ frontend/src/pages/AllOrders.jsx
@@ -229,7 +229,8 @@ const AllOrders = () => {
                       )}
                     </td>
                   </tr>
-                ))
+                  );
+                })
               )}
             </tbody>
           </table>
