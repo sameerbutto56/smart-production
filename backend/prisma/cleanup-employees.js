f5e0dffda2757202f77a6cf838e72af30bd78f7a
Sameer Butt <sameerbutt056@gmail.com>
2026-07-13 23:11:58 +0500

    Remove test scripts

--- backend/prisma/cleanup-employees.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  await prisma.outletEmployee.deleteMany({ where: { name: 'Gull', outletName: 'Johar Town' } });
-  console.log('Cleaned up duplicate Gull');
-}
-main().catch(e => console.error(e)).finally(() => prisma.$disconnect());
